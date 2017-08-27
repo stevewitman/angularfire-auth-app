b05e1d31be37a7aaa11e59367d0d5817194fc541
Steve Witman <stevewitman@gmail.com>
2017-08-26 22:50:55 -0600

    End of tutorial - anonymous login working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBoE63_A8EuiFhCx0Q_3WkNIe4iVqrun-w",
+  authDomain: "ablinks-4e960.firebaseapp.com",
+  databaseURL: "https://ablinks-4e960.firebaseio.com",
+  projectId: "ablinks-4e960",
+  storageBucket: "ablinks-4e960.appspot.com",
+  messagingSenderId: "154062695240"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
